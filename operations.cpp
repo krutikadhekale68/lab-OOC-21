dc8e051c3f651d5067543043eb7a9c9e9eb92ec2
krutikadhekale68 <krutikadhekale68@gmail.com>
2026-07-23 15:50:15 +0000

    create operations

--- operations.cpp
+++ operations.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,ch;
+
+    cout<<"Enter first number:";
+    cin >> a;
+    cout<<"Enter second number:";
+    cin >> b;
+    cout<<"\nMenu\n";
+    cout<<"1.Addition\n";
+    cout<<"2.Substraction\n";
+    cout<<"3.Multiplication\n";
+    cout<<"4.Divisiono\n";
+    cout<<"5.Modulo\n";
+    cout <<"Enter your choice:";
+    cin >> ch;
+    switch(ch)
+    {
+        case 1:
+        cout<<"Addition="<<a+b;
+        break;
+        case 2:cout<<"Substraction="<<a-b;
+        break;
+        case 3:cout<<"Multiplication="<<a*b;
+        break;
+        case 4:cout<<"Division="<<a/b;
+        break;
+        case 5:cout<<"Modulo="<<a%b;
+        break;
+        default:
+        cout<<"Invalid Choice";
+    }
+    return 0;
+}
+    
+
+    
 
