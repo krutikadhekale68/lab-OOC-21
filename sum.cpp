141e8397eb8b115376fb6a7e34c39e7d966d1f2f
krutikadhekale68 <krutikadhekale68@gmail.com>
2026-07-23 15:05:24 +0000

    create a sum program

--- sum.cpp
+++ sum.cpp
@@ -1,1 +1,11 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=10,b=20,sum;
+    sum=a+b;
+    cout<<"First Number="<<a<< endl;
+    cout<<"Second Number="<<b<< endl;
+    cout<<"Sum="<<sum;
+    return 0;
+}
