44d9821d5e8053b19e49fb67a0074477ca12e08a
krutikadhekale68 <krutikadhekale68@gmail.com>
2026-07-28 03:38:40 +0000

    create a program to display addition of three numbers

---
+++ three.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,sum;
+    printf("Enter first number:");
+    scanf("%d",&a);
+    printf("Enter second number:");
+    scanf("%d",&b);
+    printf("Enter third number:");
+    scanf("%d:",&c);
+    printf("Sum=%d",sum);
+    return 0;
+}
