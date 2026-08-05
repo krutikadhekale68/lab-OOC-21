8bc2866fe7f07f58dfd1817ad6e22ab8104b6455
krutikadhekale68 <krutikadhekale68@gmail.com>
2026-08-05 11:43:53 +0000

    overloading

---
+++ Program3.cpp
@@ -0,0 +1,27 @@
+//function area() is overloaded three times
+#include<iostream>
+using namespace std;
+//Declaration of function Prototypes
+int area(int);
+int area(int,int);
+float area(float);
+
+int main()
+{
+cout<<"Calling the area() function for computing the area of a square(side=5) - " <<area(5) <<"\n";
+cout<<"Calling the area() function for computing the area of a rectangle (length=5, breadth=10) - "<<area(5,10)<< "\n";
+cout<<"Calling the area() function for computing the area of a circle (radius = f 5.5) - "<< area(5.5);
+return 0;
+}
+int area(int side)    //Area of a square
+{
+    return(side*side);
+}  
+int area(int length,int breadth)    //Area of rectangle
+{
+return(length*breadth);
+}
+float area(float radius)       //Area of circle
+{
+    return(3.14*radius*radius);
+}
