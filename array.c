609d54ab51d88795dfa524984b591f34c11673cb
krutikadhekale68 <krutikadhekale68@gmail.com>
2026-08-05 08:12:40 +0000

    create a array program

---
+++ array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int arr[4],i,sum=0;
+    printf("Enter 4 elements:\n");
+    for(i=0;i<4;i++)
+    {
+        scanf("%d",&arr[i]);
+        sum=sum+arr[i];
+    }
+    printf("Sum of array elements=%d",sum);
+    return 0;
+}
